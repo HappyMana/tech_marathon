33401f17b4164704cad22363064a42664b4e7202
Mana Kinoshita <73377985+HappyMana@users.noreply.github.com>
2022-12-10 14:30:39 +0900

    ページ遷移を仮で実装

--- tech_marathon/urls.py
+++ tech_marathon/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('book/<int:book_id>', book.BookTemplateView.show, name='book_show'),
     path('book/new', book.BookTemplateView.new, name='book_new'),
     path('book/create', book.BookTemplateView.create, name='book_create'),
-    path('book/edit/<int:book_id>', book.BookTemplateView.edit, name='book_create'),
+    path('book/edit/<int:book_id>', book.BookTemplateView.edit, name='book_edit'),
     path('book/delete', book.BookTemplateView.delete, name='book_delete'),
     path('book/unread_books', book.BookTemplateView.unread_books, name='unread_books'),
 ]
