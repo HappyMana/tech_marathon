1b2bd601ff0d4d50f2eae643119d4ac3bff22013
Mana Kinoshita <73377985+HappyMana@users.noreply.github.com>
2022-12-10 20:08:37 +0900

    認証のフォームを作成

--- tech_marathon/forms.py
+++ tech_marathon/forms.py
@@ -6,3 +6,12 @@ class BookForm(forms.Form):
   read_status = forms.BooleanField(label="よんだか")
   begin_date = forms.DateField(label="購入日")
   end_date = forms.DateField(label="読破日")
+
+class SignInForm(forms.Form):
+  name = forms.CharField(label="名前")
+  password = forms.CharField(label="パスワード", widget=forms.PasswordInput(), min_length=8)
+
+class SignUpForm(forms.Form):
+  name = forms.CharField(label="名前")
+  email = forms.ChoiceField(label="メールアドレス")
+  password = forms.CharField(label="パスワード", widget=forms.PasswordInput(), min_length=8)
