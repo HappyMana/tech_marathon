fc56f5d65c0cf46a0b0ea325b8bb081ded376a14
Mana Kinoshita <73377985+HappyMana@users.noreply.github.com>
2022-12-11 00:35:33 +0900

    modelを更新

--- tech_marathon/models.py
+++ tech_marathon/models.py
@@ -6,17 +6,14 @@ class User(models.Model):
   email = models.CharField(max_length=30)
   goal = models.CharField(max_length=200)
 
+class Category(models.Model):
+  name = models.CharField(max_length=20)
+
 class Book(models.Model):
   title = models.CharField(max_length=50)
   memo = models.CharField(max_length=200)
   read_status = models.BooleanField(default=False)
   begin_date = models.DateTimeField('purchased date')
   end_date = models.DateTimeField('finished reading date')
-  user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-
-class Category(models.Model):
-  name = models.CharField(max_length=20)
-
-class BookCategory(models.Model):
-  book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
-  category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  category = models.ManyToManyField(Category, related_name='book')
