9d88d852a1f2bc310d04aea1558863f112a06602
Fujikouki <c1202599@st.kanazawa-it.ac.jp>
2022-12-11 13:27:50 +0900

    編集

--- tech_marathon/forms.py
+++ tech_marathon/forms.py
@@ -1,18 +1,12 @@
 from django import forms
+from .models import Book
 
 class BookForm(forms.Form):
   title = forms.CharField(label="タイトル")
   memo = forms.CharField(label="メモ")
-  read_status = forms.BooleanField(label="よんだか")
+  # read_status = forms.BooleanField(label="よんだか")
   begin_date = forms.DateField(label="購入日", widget=forms.SelectDateWidget())
   end_date = forms.DateField(label="読破日", widget=forms.SelectDateWidget())
-  category = forms.CharField(label="カテゴリー")
+  # category = forms.CharField(label="カテゴリー")
 
-class SignInForm(forms.Form):
-  name = forms.CharField(label="名前")
-  password = forms.CharField(label="パスワード", widget=forms.PasswordInput(), min_length=8)
 
-class SignUpForm(forms.Form):
-  name = forms.CharField(label="名前")
-  email = forms.CharField(label="メールアドレス")
-  password = forms.CharField(label="パスワード", widget=forms.PasswordInput(), min_length=8)
